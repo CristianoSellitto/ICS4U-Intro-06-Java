94b1484fef3821813bae1d270d44b867456f94ba
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2024-02-14 16:32:26 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
-/**
+/*
 * The HelloWorld program implements an application that
 * simply displays "Hello World!" to the standard output.
 *
 * @author  Cristiano Sellitto
 * @version 1.0
 * @since   2024-02-14;
-*/ 
+*/
 
 /**
 * This is the standard "Hello, World!" program.
